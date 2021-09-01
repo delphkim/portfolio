13ad99fc5bea1ced3051801f9509c0cb8cd937bf
develPhaba <hgkim9201@gmail.com>
2021-09-01 19:01:55 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ document.addEventListener('scroll', () => {
     navbar.classList.remove('navbar--dark');
   }
 });
+
+// handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (!link) {
+    return;
+  } else {
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  }
+});
